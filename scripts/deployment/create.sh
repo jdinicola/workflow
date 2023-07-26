03801a8328e8598e9ac09d79571a5115acd0fac3
Julio Di Nicola <juliodinicola@gmail.com>
2023-07-27 00:36:48 +0200

    script

--- scripts/deployment/create.sh
+++ scripts/deployment/create.sh
@@ -1,1 +1,15 @@
-echo "create zip..."
+#!/bin/sh
+
+mkdir dist && zip dist/bundle.zip -r . -x \
+.\* \
+node_modules/\* \
+coverage/\* \
+scripts/\* \
+mocks/\* \
+dist/\* \
+src/\* \
+npm-debug.log \
+package\*.json \
+\*.md \
+\*/\*.test.jsx \
+\*/\*.test.js \
