7b81814e9ffb5bc02a734a1ab97c0fbe2a1689f1
Julio Di Nicola <juliodinicola@gmail.com>
2023-08-25 10:14:31 +0200

    fix

--- scripts/coverage-summary.sh
+++ scripts/coverage-summary.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#COVERAGE_THRESHOLD="65,85"
-
 TOTAL_COVERAGE=$(jq '[.total[] | if .pct == "Unknown" then 0 else .pct end] | add / length | tonumber | round' $PWD/coverage/coverage-summary.json)
 
 IFS=', ' read -r -a THRESHOLD <<< "$COVERAGE_THRESHOLD"
@@ -20,10 +18,10 @@ OUTPUT+=":--- | :--- | :--- | :--- | :---"
 OUTPUT+="\n"
 OUTPUT+="![Coverage](https://badgers.space/badge/Coverage/${TOTAL_COVERAGE}%25/${STATUS}) | "
 OUTPUT+=$(jq -r '[
-  "**\(.total.lines.pct | tonumber | round)** (\(.total.lines.covered)/\(.total.lines.total))",
-  "**\(.total.statements.pct | tonumber | round)** (\(.total.statements.covered)/\(.total.statements.total))",
-  "**\(.total.functions.pct | tonumber | round)** (\(.total.functions.covered)/\(.total.functions.total))",
-  "**\(.total.branches.pct | tonumber | round)** (\(.total.branches.covered)/\(.total.branches.total))"
+  "**\(.total.lines.pct | tonumber | round)%** (\(.total.lines.covered)/\(.total.lines.total))",
+  "**\(.total.statements.pct | tonumber | round)%** (\(.total.statements.covered)/\(.total.statements.total))",
+  "**\(.total.functions.pct | tonumber | round)%** (\(.total.functions.covered)/\(.total.functions.total))",
+  "**\(.total.branches.pct | tonumber | round)%** (\(.total.branches.covered)/\(.total.branches.total))"
 ] | map(tostring) | join(" | ")' $PWD/coverage/coverage-summary.json)
 OUTPUT+="\n\n"
 OUTPUT+="_Minimum allowed coverage is \`${THRESHOLD[0]}%\`_"
