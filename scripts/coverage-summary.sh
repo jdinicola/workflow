52de030557952bebddaf5706694c09fd30bd0914
Julio Di Nicola <juliodinicola@gmail.com>
2023-08-25 09:07:10 +0200

    fix

--- scripts/coverage-summary.sh
+++ scripts/coverage-summary.sh
@@ -16,10 +16,10 @@ OUTPUT="![Coverage](https://badgers.space/badge/Coverage/${TOTAL_COVERAGE}%25/${
 OUTPUT+="\n\n"
 OUTPUT+=$(jq -r '["Metric", "Total", "Covered", "Skipped", "%"],
 [":---", ":---", ":---", ":---", ":---"],
-["Lines", .total.lines.total, .total.lines.covered, .total.lines.skipped, .total.lines.pct | tonumber | round],  
- ["Statements", .total.statements.total, .total.statements.covered, .total.statements.skipped, .total.statements.pct | tonumber | round],  
- ["Functions", .total.functions.total, .total.functions.covered, .total.functions.skipped, .total.functions.pct | tonumber | round],  
- ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, .total.branches.pct | tonumber | round] | map(tostring) | join(" | ")' $PWD/coverage/coverage-summary.json)
+["Lines", .total.lines.total, .total.lines.covered, .total.lines.skipped, (.total.lines.pct | tonumber | round)],  
+ ["Statements", .total.statements.total, .total.statements.covered, .total.statements.skipped, (.total.statements.pct | tonumber | round)],  
+ ["Functions", .total.functions.total, .total.functions.covered, .total.functions.skipped, (.total.functions.pct | tonumber | round)],  
+ ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, (.total.branches.pct  | tonumber | round)] | map(tostring) | join(" | ")' $PWD/coverage/coverage-summary.json)
 OUTPUT+="\n\n"
 OUTPUT+="_Minimum allowed coverage is \`${THRESHOLD[0]}%\`_"
 
