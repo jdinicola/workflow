8be7a52132039cb3b48c3184c004f352580aa00c
Julio Di Nicola <juliodinicola@gmail.com>
2023-08-25 08:45:19 +0200

    fix: fix

--- scripts/coverage-summary.sh
+++ scripts/coverage-summary.sh
@@ -13,12 +13,12 @@ else
 fi
 
 OUTPUT="![Coverage](https://badgers.space/badge/Coverage/${TOTAL_COVERAGE}%25/${STATUS})"
-OUTPUT+=jq -r '["Metric", "Total", "Covered", "Skipped", "%"],
+OUTPUT+=$(jq -r '["Metric", "Total", "Covered", "Skipped", "%"],
 ["---", "---", "---", "---", "---"],
 ["Lines", .total.lines.total, .total.lines.covered, .total.lines.skipped, .total.lines.pct],  
  ["Statements", .total.statements.total, .total.statements.covered, .total.statements.skipped, .total.statements.pct],  
  ["Functions", .total.functions.total, .total.functions.covered, .total.functions.skipped, .total.functions.pct],  
- ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, .total.branches.pct] | map(tostring) | join(" | ")' $PWD/coverage/coverage-summary.json
+ ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, .total.branches.pct] | map(tostring) | join(" | ")' $PWD/coverage/coverage-summary.json)
 OUTPUT+=" "
 OUTPUT+="_Minimum allowed coverage is \`${THRESHOLD[0]}%\`_"
 
