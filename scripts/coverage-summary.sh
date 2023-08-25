a3a97e784a7fb0e73335e0e614cc4992d800de66
Julio Di Nicola <juliodinicola@gmail.com>
2023-08-25 08:16:30 +0200

    fix: update workflow

--- scripts/coverage-summary.sh
+++ scripts/coverage-summary.sh
@@ -12,15 +12,14 @@ else
     STATUS="green"
 fi
 
-OUTPUT="### Code coverage summary"
-OUTPUT+="![Coverage](https://badgers.space/badge/Coverage/${TOTAL_COVERAGE}%25/${STATUS})" && \
+OUTPUT="![Coverage](https://badgers.space/badge/Coverage/${TOTAL_COVERAGE}%25/${STATUS})" && \
 jq -r '["Metric", "Total", "Covered", "Skipped", "%"],
 ["---", "---", "---", "---", "---"],
 ["Lines", .total.lines.total, .total.lines.covered, .total.lines.skipped, .total.lines.pct],  
  ["Statements", .total.statements.total, .total.statements.covered, .total.statements.skipped, .total.statements.pct],  
  ["Functions", .total.functions.total, .total.functions.covered, .total.functions.skipped, .total.functions.pct],  
- ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, .total.branches.pct] | map(tostring) | join(" | ")' coverage/coverage-summary.json
+ ["Branches", .total.branches.total, .total.branches.covered, .total.branches.skipped, .total.branches.pct] | map(tostring) | join(" | ")' ../coverage/coverage-summary.json
 OUTPUT+=" "
 OUTPUT+="_Minimum allowed coverage is \`${THRESHOLD[0]}%\`_"
 
-echo "$OUTPUT" >> $GITHUB_STEP_SUMMARY
+echo -e "$OUTPUT" >> $GITHUB_STEP_SUMMARY
