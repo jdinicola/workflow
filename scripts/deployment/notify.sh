5acc7a93cc20c129961f937744f401b4cccd2422
Julio Di Nicola <juliodinicola@gmail.com>
2023-08-31 16:08:34 +0200

    [SERS-1025] [Didomi] Modificar lógica para obtener el estado del consentimiento de youbora

--- scripts/deployment/notify.sh
+++ scripts/deployment/notify.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
-echo "notify..."
+PAYLOAD='{
+    "@type": "MessageCard",
+    "@context": "https://schema.org/extensions",
+    "summary": "Despliegue",
+    "themeColor": "009f4a",
+    "title": "'$REPOSITORY'",
+    "sections": [{
+      "activityTitle": "'$(echo $COMMIT_MESSAGE | sed "s/\[/\\[/g; s/\]/\\]/g")'",
+      "activitySubtitle": "Por '$TRIGGERING_ACTOR'", 
+      "facts": [{
+        "name": "Bundle     ",
+        "value": "'$BUNDLE_NAME'"
+      },
+      {
+        "name": "Versión    ",
+        "value": "'$DEPLOYED_VERSION'"
+      },
+      {
+        "name": "PageBuilder",
+        "value": "'$PAGEBUILDER_VERSION'"
+      }]
+    }],
+    "potentialAction": [{
+      "@type": "OpenUri",
+      "name": "Ver workflow",
+      "targets": [{
+        "os": "default",
+        "uri": "'$SERVER_URL'/'$REPOSITORY'/actions/runs/'$RUN_ID'"
+      }]
+    },'$([ ! -z "$PULL_REQUEST_NUMBER" ] && echo '{
+      "@type": "OpenUri",
+      "name": "Ver pull request",
+      "targets": [{
+        "os": "default",
+        "uri": "'$SERVER_URL'/'$REPOSITORY'/pull/'$PULL_REQUEST_NUMBER'"
+      }]
+    },')'
+    {
+      "@type": "OpenUri",
+      "name": "Ver despliegue",
+      "targets": [{
+        "os": "default",
+        "uri": "'$DEPLOYMENT_URL'"
+      }]
+    }]
+  }'
 
-echo "$BUNDLE_NAME"
-echo "$DEPLOYED_VERSION"
-echo "$PULL_REQUEST_NUMBER"
-echo "$COMMIT_MESSSAGE"
-echo "$DEPLOYMENT_URL"
+echo "$PAYLOAD"
