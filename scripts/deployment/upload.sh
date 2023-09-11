518e5ab964145e297339b3a297d4ad60e24bcb1b
Julio Di Nicola <juliodinicola@gmail.com>
2023-09-11 16:10:02 +0200

    fix: fix [skip ci]

--- scripts/deployment/upload.sh
+++ scripts/deployment/upload.sh
@@ -2,9 +2,7 @@
 
 echo "upload bundle..."
 
-NAME=$(git rev-parse --short $GITHUB_SHA)
-
-[[ $GITHUB_EVENT_NAME = 'workflow_dispatch' ]] && NAME="$GITHUB_RUN_ID" || NAME="$(git rev-parse --short $GITHUB_SHA)"
+[[ $GITHUB_EVENT_NAME = 'workflow_dispatch' ]] && NAME="wd_$GITHUB_RUN_ID" || NAME="$(git rev-parse --short $GITHUB_SHA)"
 
 echo $NAME
 
